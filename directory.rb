30046d34b9756b9a6b1858ed773520a3f3d12692
matt jones <mattjosephjones@gmail.com>
2023-02-27 17:05:34 +0000

    Changed student list to array

--- directory.rb
+++ directory.rb
@@ -1,20 +1,25 @@
-# Assign value to variable
-student_count = 11
+# Assign students to array
+student_list = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Rached",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Jofferey Baratheon",
+  "Norman Bates",
+]
 
-# Print list of students, each name on a new line.
-puts "The Students of Villians Academy"
-puts "--------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Rached"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Jofferey Baratheon"
-puts "Norman Bates"
+# Print header and line at the top of the list.
+puts "The Students of Villians Academy\n----------------------------"
+
+# prints each name in student list array to new line.
+student_list.each { |student|
+  puts(student)
+}
 
 # Prints student count to new line.
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{student_list.length} great students\n"
